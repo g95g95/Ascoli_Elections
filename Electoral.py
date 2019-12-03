c0d5480326edd7ecf1160e24e875002aebc122c6
Giulio Bonelli <giulio.bonelli@studio.unibo.it>
2019-12-03 21:47:34 +0100

    Primary version of the header file of the ascoli elections simulator

---
+++ Electoral.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 21:36:52 2019
+
+@author: Giulio
+"""
+
+import random as rd
+import numpy as np
+
+class voter:
+	def __init__(self,age,sex,work,pos,chessboard):
+		self.age = age
+		self.sex = sex
+		self.work= work
+		self.chessboard = chessboard
+		self.pos = pos
+	def Make_Move(self,self.pos,lenmove = 0.05)
+		
+	
+	
